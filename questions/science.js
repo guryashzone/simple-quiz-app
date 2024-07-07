13aa24096f71dff68fdc554794835b9a8f8a699b
guryashzone <46774163+guryashzone@users.noreply.github.com>
2024-07-08 01:10:13 +0530

    Initial Commit

---
+++ questions/science.js
@@ -0,0 +1,92 @@
+var science_questions = [
+  {
+    "no": 1,
+    "question": "Science 1",
+    "option_1": "Parrot",
+    "option_2": "Carrot",
+    "option_3": "Zebra",
+    "option_4": "Peackok",
+    "correct_option": 4
+  },
+  {
+    "no": 2,
+    "question": "What is national bird of India?",
+    "option_1": "Parrot",
+    "option_2": "Carrot",
+    "option_3": "Zebra",
+    "option_4": "Peackok",
+    "correct_option": 4
+  },
+  {
+    "no": 3,
+    "question": "What is national bird of India?",
+    "option_1": "Parrot",
+    "option_2": "Carrot",
+    "option_3": "Zebra",
+    "option_4": "Peackok",
+    "correct_option": 4
+  },
+  {
+    "no": 4,
+    "question": "What is national bird of India?",
+    "option_1": "Parrot",
+    "option_2": "Carrot",
+    "option_3": "Zebra",
+    "option_4": "Peackok",
+    "correct_option": 4
+  },
+  {
+    "no": 5,
+    "question": "What is national bird of India?",
+    "option_1": "Parrot",
+    "option_2": "Carrot",
+    "option_3": "Zebra",
+    "option_4": "Peackok",
+    "correct_option": 4
+  },
+  {
+    "no": 6,
+    "question": "What is national bird of India?",
+    "option_1": "Parrot",
+    "option_2": "Carrot",
+    "option_3": "Zebra",
+    "option_4": "Peackok",
+    "correct_option": 4
+  },
+  {
+    "no": 7,
+    "question": "What is national bird of India?",
+    "option_1": "Parrot",
+    "option_2": "Carrot",
+    "option_3": "Zebra",
+    "option_4": "Peackok",
+    "correct_option": 4
+  },
+  {
+    "no": 8,
+    "question": "What is national bird of India?",
+    "option_1": "Parrot",
+    "option_2": "Carrot",
+    "option_3": "Zebra",
+    "option_4": "Peackok",
+    "correct_option": 4
+  },
+  {
+    "no": 9,
+    "question": "What is national bird of India?",
+    "option_1": "Parrot",
+    "option_2": "Carrot",
+    "option_3": "Zebra",
+    "option_4": "Peackok",
+    "correct_option": 4
+  },
+  {
+    "no": 10,
+    "question": "What is national bird of India?",
+    "option_1": "Parrot",
+    "option_2": "Carrot",
+    "option_3": "Zebra",
+    "option_4": "Peackok",
+    "correct_option": 4
+  }
+];
